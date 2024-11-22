85a427428f4dfb07c3893101e9de4cbdca798193
Мария Тарлавина <tarlavina50@mail.ru>
2024-11-22 20:28:47 +0700

    class work

---
+++ CountryDictionaryApiApp/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CountryDictionaryApiApp
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string useConnection = configuration.GetSection("UseDbConnection").Value ?? "DefaultConnection";
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString(useConnection));
+        }
+
+    }
+}
--- CountryDictionaryApiApp/Program.cs
+++ CountryDictionaryApiApp/Program.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using CountryDictionaryApiApp;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>();
 var app = builder.Build();
 
 app.MapGet("/", () => new
@@ -16,5 +19,15 @@ app.MapGet("/ping", () => new
 {
     Message = "ping"
 });
+
+// countries api
+app.MapGet("/country", async (ApplicationDbContext db) => await db.Countries.ToListAsync());
+app.MapPost("/country", async (Country country, ApplicationDbContext db) =>
+{
+    await db.Countries.AddAsync(country);
+    await db.SaveChangesAsync();
+    return country;
+});
+
 
 app.Run();
